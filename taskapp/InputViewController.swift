ac88676a680751fcabd8bb6407adf0abfaaba6c4
taiki_hamaguchi <taiki_hamaguchi@jrass.jp>
2021-03-23 14:51:30 +0900

    Realmをインストールした

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by PC-SYSKAI553 on 2021/03/23.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBOutlet weak var titletextfield: UITextField!
+    @IBOutlet weak var contentstextview: UITextView!
+    @IBOutlet weak var datapicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //セルタップで呼ばれるSegueを指定
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        
+        //UITableViewCell.EditingStyleプロパティにdeleteを設定、列挙型を参照
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
 
 }
 
