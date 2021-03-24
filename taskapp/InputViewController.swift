750b46cb3107985cfe2e1f7ca402389115050a0b
taiki_hamaguchi <taiki_hamaguchi@jrass.jp>
2021-03-24 10:36:32 +0900

    通知以外完成

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -6,16 +6,46 @@
 //
 
 import UIKit
+import RealmSwift
+
 
 class InputViewController: UIViewController {
     @IBOutlet weak var titletextfield: UITextField!
     @IBOutlet weak var contentstextview: UITextView!
     @IBOutlet weak var datapicker: UIDatePicker!
     
+    let realm = try! Realm()
+    var task: Task!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        //viewプロパティにtapGestureを追加
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titletextfield.text = task.title
+        contentstextview.text = task.contents
+        datapicker.date = task.date
+    }
+    
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titletextfield.text!
+            self.task.contents = self.contentstextview.text!
+            self.task.date = self.datapicker.date
+            self.realm.add(self.task, update: .modified)
+        }
+
+        super.viewWillDisappear(animated)
     }
     
 
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -6,10 +6,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    //Realmインスタンスをrealmに格納。（個人データを編集するのに使用）
+    let realm = try! Realm()
+    
+    // DB内のタスクが格納される配列。
+    // 日付の近い順でソート：昇順
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,14 +27,52 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    // 入力画面から戻ってきた時に TableView を更新させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //InputVCに接続
+        let inputVC: InputViewController = segue.destination as! InputViewController
+        
+        //セルタップの場合の値受け渡し
+        if segue.identifier == "cellSegue" {
+            //セル位置情報を取得、格納
+            let indexPath = tableView.indexPathForSelectedRow
+            //セル位置に応じたデータをinputVCのtaskに格納
+            inputVC.task = taskArray[indexPath!.row]
+        } else {            //追加時の値受け渡し
+            let task = Task()
+            let alltasks = realm.objects(Task.self)
+            //すでにデータがある場合はidに+1する
+            if alltasks.count != 0 {
+                task.id = alltasks.max(ofProperty: "id")! + 1
+            }
+            //idを更新した状態でinputVCのtaskに格納
+            inputVC.task = task
+        }
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return taskArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //取り出したデータをtaskに格納
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
         
         return cell
         
@@ -45,6 +92,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
+        if editingStyle == .delete {
+            // データベースから削除する
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
     }
 
 }
